188fa49aa863925b80177f682e01f03df804b011
aamir-syed93 <aamirhusain.syed@gmail.com>
2017-10-07 19:44:45 +0530

    method removed
    removed sms method

--- src/main/java/com/bma/util/SystemUtils.java
+++ src/main/java/com/bma/util/SystemUtils.java
@@ -32,45 +32,5 @@ public class SystemUtils {
 	}
 */
 	
-	public static void sendSms(String phoneNumber,String message)
-	{
-		  System.out.println(phoneNumber);
-		  System.out.println(message);
-
-		 String twilioHost = "api.twilio.com";
-		 final String ACCOUNT_SID = "ACf38638d15310ac74072df44b27da3515";
-		 final String AUTH_TOKEN = "c148ee397607a8f8b20bea4d3bd73248";
-		  
-		/* final String Account_Username = "irtspl";
-		 final String Account_Password = "irtspl1234";*/
-		
-		 org.apache.commons.httpclient.auth.AuthScope authScope = new org.apache.commons.httpclient.auth.AuthScope("api.twilio.com", 443);
-		 HostConfiguration hc = new HostConfiguration();
-		 hc.setHost(twilioHost, 443, "https");
-		// String url = "https://api.twilio.com/2010-04-01/Accounts/ACf38638d15310ac74072df44b27da3515/Messages.json";
-		 
-		 String url ="http://203.212.70.200/smpp/sendsms?username=irtspl&password=irtspl1234";
-		 
-		 
-		 HttpClient client = new HttpClient();
-		 Credentials c = new UsernamePasswordCredentials(ACCOUNT_SID, AUTH_TOKEN);
-		 client.getState().setCredentials(authScope, c);
-		 
-		 PostMethod p = new PostMethod(url);
-		 p.addParameter("from","+19073121074");
-		 p.addParameter("to","+91"+phoneNumber);
-		 p.addParameter("text",message);
-		 p.setDoAuthentication(true);
-         System.out.println(p.getParameters());
-   
-		    try {
-				client.executeMethod(hc,p);
-				p.releaseConnection();
-			} catch (HttpException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	}
-
+	
 }
